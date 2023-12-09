543b6ba3a105a906bfc0f859817ae62743e1f4a6
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2023-12-08 22:10:41 -0600

    Added db migration to remove Flow references from Queue inbound configs

--- Didact/Migrations/20231209040238_RemoveFlowRefsFromQueueInboundConfigurations.Designer.cs
+++ Didact/Migrations/20231209040238_RemoveFlowRefsFromQueueInboundConfigurations.Designer.cs
@@ -4,6 +4,7 @@ using DidactEngine.Services.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DidactEngine.Migrations
 {
     [DbContext(typeof(DidactDbContext))]
-    partial class DidactDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231209040238_RemoveFlowRefsFromQueueInboundConfigurations")]
+    partial class RemoveFlowRefsFromQueueInboundConfigurations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
