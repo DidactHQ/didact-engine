7f36e52fc33620eeb575ac26542dbc3e76006fea
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2023-12-10 02:26:22 -0600

    Created WorkerBackgroundService

---
+++ Didact/Services/BackgroundServices/WorkerBackgroundService.cs
@@ -0,0 +1,32 @@
+﻿namespace DidactEngine.Services.BackgroundServices
+{
+    public class WorkerBackgroundService : BackgroundService
+    {
+        private readonly ILogger<WorkerBackgroundService> _logger;
+
+        public WorkerBackgroundService(ILogger<WorkerBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Starting {name}...", nameof(WorkerBackgroundService));
+
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Ping from the {name}.", nameof(WorkerBackgroundService));
+                    await Task.Delay(5000);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical("{name} failed with the following exception:{nl}{exception}",
+                    nameof(WorkerBackgroundService), Environment.NewLine, ex);
+                throw;
+            }
+        }
+    }
+}
