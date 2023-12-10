7babf6a7fe6570d84c71627b34e566b783d7b8e3
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2023-12-10 02:07:03 -0600

    Created EngineConfiguration

---
+++ Didact/Services/Contexts/Configurations/EngineConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using DidactEngine.Models.Entities;
+
+namespace DidactEngine.Services.Contexts.Configurations
+{
+    public partial class EngineConfiguration : IEntityTypeConfiguration<Engine>
+    {
+        public void Configure(EntityTypeBuilder<Engine> entity)
+        {
+            entity.ToTable(nameof(Engine));
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(255);
+            entity.Property(e => e.UniqueName).IsRequired().HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.LastUpdatedBy).HasMaxLength(255);
+            entity.Property(e => e.Active).IsRequired().HasDefaultValue(true);
+            entity.Property(e => e.RowVersion).IsRowVersion().IsConcurrencyToken();
+
+            entity.HasOne(d => d.Organization)
+                .WithMany(p => p.Engines)
+                .HasForeignKey(d => d.OrganizationId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName($"FK_{nameof(Engine)}_{nameof(Organization)}");
+
+            OnConfigurePartial(entity);
+        }
+
+        partial void OnConfigurePartial(EntityTypeBuilder<Engine> entity);
+    }
+}
