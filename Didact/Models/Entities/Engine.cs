7c932b7f91f848b49e131662d84b74c97331f357
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2023-12-10 02:01:37 -0600

    Created Engine entity

---
+++ Didact/Models/Entities/Engine.cs
@@ -0,0 +1,29 @@
+﻿namespace DidactEngine.Models.Entities
+{
+    public class Engine
+    {
+        public int EngineId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string UniqueName { get; set; } = null!;
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; }
+
+        public string LastUpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public byte[] RowVersion { get; set; } = null!;
+
+        public virtual Organization Organization { get; set; } = null!;
+    }
+}
