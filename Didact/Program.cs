9f3cf57759ce11bfff82640ac32d37b3ddc0f6cd
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-01-07 03:34:56 -0600

    Cleaned code

--- Didact/Program.cs
+++ Didact/Program.cs
@@ -1,16 +1,11 @@
-using DidactCore.Flows;
 using DidactEngine.Hubs;
-using DidactEngine.Services;
 using DidactEngine.Services.BackgroundServices;
 using DidactEngine.Services.Contexts;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
-using System.Runtime.Loader;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -74,51 +69,7 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddEndpointsApiExplorer();
 
 // Register BackgroundServices
-//builder.Services.AddHostedService<DatabaseEngineBackgroundService>();
-//builder.Services.AddHostedService<AssemblyReaderBackgroundService>();
 builder.Services.AddHostedService<WorkerBackgroundService>();
-
-builder.Services.AddSingleton<ExecutionManager>();
-builder.Services.AddSignalR();
-
-#region Add Flow Repository
-
-//var flowRepositoryBackgroundServiceHashCode = typeof(FlowRepositoryBackgroundService).GetHashCode();
-//var aqn = typeof(FlowRepositoryBackgroundService).FullName;
-//Console.WriteLine($"See the hash code below:\n\n{flowRepositoryBackgroundServiceHashCode}");
-//Console.WriteLine($"\n\nThe assembly qualified name is:\n\n{aqn}");
-
-//var currentAssembly = Assembly.GetExecutingAssembly();
-//var test = currentAssembly.GetTypes().FirstOrDefault(t => t.AssemblyQualifiedName == aqn);
-//Console.WriteLine($"Test:\n\n{test.AssemblyQualifiedName}");
-
-using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
-    .SetMinimumLevel(LogLevel.Trace)
-    .AddConsole());
-
-ILogger _logger = loggerFactory.CreateLogger<Program>();
-
-var flowLibraryAssembly = Assembly.LoadFile(@"C:\Users\falco\source\repos\didact-core\DidactCore\bin\Debug\netstandard2.1\DidactCore.dll");
-var flowTypes2 = flowLibraryAssembly.GetTypes().Where(t => t.GetInterface("IFlow") is not null);
-foreach (var type in flowTypes2)
-{
-    if (true)
-    {
-        var interfaces = type.GetInterfaces().Select(i => i.Name).ToList();
-        var interfacesString = string.Join(", ", interfaces);
-        _logger.LogInformation("The type name is: {type}", type.Name);
-        _logger.LogInformation("The type's interfaces are: {interfaces}", interfacesString);
-    }
-
-    var method = type.GetMethod("ExecuteAsync");
-    var assemblyVersion = flowLibraryAssembly.GetName().Version;
-    //var x = ActivatorUtilities.CreateInstance(builder.Build().Services, type);
-}
-
-//var someFlowType = flowLibraryAssembly.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(IFlow))).FirstOrDefault();
-//_logger.LogInformation("Here is an IFlow implemented class: {class}", someFlowType.Name);
-
-#endregion
 
 var app = builder.Build();
 
