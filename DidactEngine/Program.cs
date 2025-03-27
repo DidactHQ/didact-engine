92b6876a38bfa295e97692acaf6fc22a42223b09
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-03-27 02:32:10 -0500

    Added Spectre.Console markup techniques

--- DidactEngine/Program.cs
+++ DidactEngine/Program.cs
@@ -4,8 +4,59 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.OpenApi.Models;
+using Spectre.Console;
 using System.Reflection;
-using DidactCore.Flows;
+
+#region App metadata
+
+var applicationName = "Didact Engine";
+var themeColor = new Color(249, 115, 22);
+var assembly = Assembly.GetExecutingAssembly();
+var assemblyName = assembly.GetName().Name;
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var settingsFilename = "enginesettings.json";
+var apiBasePath = "/api";
+var urls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+var urlsSplit = string.IsNullOrEmpty(urls) ? [] : urls.Split(';');
+var consoleUrls = string.Empty;
+foreach (var url in urlsSplit)
+{
+    var separator = string.IsNullOrEmpty(consoleUrls) ? string.Empty : Environment.NewLine;
+    consoleUrls = string.Concat(consoleUrls, separator, url, apiBasePath);
+}
+
+#endregion
+
+#region Configure Spectre Console decorator.
+
+var figletText = new FigletText(applicationName).LeftJustified().Color(themeColor);
+AnsiConsole.Write(figletText);
+
+// Create a table
+var table = new Table().HideHeaders();
+table.AddColumn("");
+table.AddColumn(new TableColumn(""));
+table.AddRow("Name", applicationName);
+table.AddRow("Version", assembly.GetName().Version!.ToString());
+table.AddRow("Start time", DateTime.UtcNow.ToString("O"));
+table.AddRow("Process Id", Environment.ProcessId.ToString());
+table.AddRow("OS version", Environment.OSVersion.ToString());
+table.AddRow("Machine name", Environment.MachineName);
+table.AddRow("Username", Environment.UserName);
+table.AddRow("Environment", environment ?? string.Empty);
+if (!string.IsNullOrEmpty(consoleUrls))
+    table.AddRow("API", consoleUrls);
+table.BorderStyle(new Style(themeColor));
+
+var padder = new Padder(table).PadBottom(1).PadTop(0);
+
+var grid = new Grid();
+grid.AddColumn();
+grid.AddRow(padder);
+
+AnsiConsole.Write(grid);
+
+#endregion
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -69,7 +120,7 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddEndpointsApiExplorer();
 
 // Register BackgroundServices
-builder.Services.AddHostedService<WorkerBackgroundService>();
+//builder.Services.AddHostedService<WorkerBackgroundService>();
 // Register Flow helper services from DidactCore.
 //builder.Services.AddSingleton<IFlowExecutor, FlowExecutor>();
 // Register repositories from DidactCore.
@@ -89,29 +140,31 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
+app.Run();
 
-try
-{
-    using (var scope = app.Services.CreateScope())
-    using (var dbContext = scope.ServiceProvider.GetRequiredService<DidactDbContext>())
-    try
-    {
-        logger.LogInformation("Attempting to migrate the database on engine startup...");
-        dbContext.Database.Migrate();
-        logger.LogInformation("Database migrated successfully.");
-    }
-    catch (Exception ex)
-    {
-        logger.LogError(ex, "Unhandled exception while applying migrations for {T}", typeof(DidactDbContext));
-        throw;
-    }
+//var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
-    app.Run();
-    return 0;
-}
-catch (Exception ex)
-{
-    logger.LogCritical(ex, "An unhandled exception occurred during bootstrapping");
-    return 1;
-}
+//try
+//{
+//    using (var scope = app.Services.CreateScope())
+//    using (var dbContext = scope.ServiceProvider.GetRequiredService<DidactDbContext>())
+//    try
+//    {
+//        logger.LogInformation("Attempting to migrate the database on engine startup...");
+//        dbContext.Database.Migrate();
+//        logger.LogInformation("Database migrated successfully.");
+//    }
+//    catch (Exception ex)
+//    {
+//        logger.LogError(ex, "Unhandled exception while applying migrations for {T}", typeof(DidactDbContext));
+//        throw;
+//    }
+
+//    app.Run();
+//    return 0;
+//}
+//catch (Exception ex)
+//{
+//    logger.LogCritical(ex, "An unhandled exception occurred during bootstrapping");
+//    return 1;
+//}
